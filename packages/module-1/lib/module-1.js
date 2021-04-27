5a5ec92b39c62ed80674bc39c4d6a351d03a3a0f
vitebo <vitebo@hotmail.com>
2021-04-27 15:50:10 -0300

    next

--- packages/module-1/lib/module-1.js
+++ packages/module-1/lib/module-1.js
@@ -3,5 +3,5 @@
 module.exports = module1;
 
 function module1() {
-    console.log('module 1');
+    console.log('module 1', { v: 1 });
 }
--- packages/module-2/lib/module-2.js
+++ packages/module-2/lib/module-2.js
@@ -3,5 +3,5 @@
 module.exports = module2;
 
 function module2() {
-    console.log('module 2');
+    console.log('module 2', { v: 2 });
 }
